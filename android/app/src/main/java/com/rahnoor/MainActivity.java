34958348544549a7d86dc6f0da6ebfd258f60f9c
rahenoor <mohammadhamzeh037@gmail.com>
2026-07-09 22:19:50 +0330

    Create MainActivity.java

---
+++ android/app/src/main/java/com/rahnoor/MainActivity.java
@@ -0,0 +1,27 @@
+package com.rahnoor;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+
+public class MainActivity extends Activity {
+
+    WebView webView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+
+        WebSettings settings = webView.getSettings();
+
+        settings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("file:///android_asset/index.html");
+
+        setContentView(webView);
+    }
+}
